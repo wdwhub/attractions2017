8873ad522079b7ac916bb8e3b49305b1ba1b178a
captproton <carl@wdwhub.net>
2017-10-29 14:36:13 -0700

    CHANGED: WIP attractions show page

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -22,9 +22,8 @@ class AttractionsController < ApplicationController
 
   # GET /venues/1
   def show
-    # @attraction        = Attraction.find(params[:id])
-    # @photos       = @venue.foursquare_eatery.find_cached_images
-    # @tips         = @venue.foursquare_eatery.find_cached_tips
+    # @photos       = @venue.foursquare_review.find_cached_images
+    # @tips         = @venue.foursquare_review.find_cached_tips
     # @dfb_articles = @venue.dfb_articles
     # @dfb_eatery   = @venue.dfb_eateries.first
   end
@@ -36,8 +35,4 @@ class AttractionsController < ApplicationController
       @attraction = Attraction.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    # def venue_params
-    #   params.require(:venue).permit(:name, :permalink, :category_code, :portion_size, :cost_code, :cuisine, :phone_number, :entree_range, :when_to_go, :parking, :bar, :wine_list, :dress, :awards, :breakfast_hours, :lunch_hours, :dinner_hours, :house_specialties, :counter_quality_rating, :counter_value_rating, :table_quality_rating, :table_value_rating, :overall_rating, :service_rating, :friendliness_rating, :thumbs_up, :adult_breakfast_menu_url, :adult_lunch_menu_url, :adult_dinner_menu_url, :child_breakfast_menu_url, :child_lunch_menu_url, :child_dinner_menu_url, :requires_credit_card, :requires_pre_payment, :extinct_on, :opened_on, :disney_permalink, :code, :short_name, :accepts_tiw, :accepts_reservations, :kosher_available, :location_details, :operator_id, :operator_url, :operator_type)
-    # end
 end
